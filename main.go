4cb6d16813027fc5fb15def4067e1085d4855d77
sus12 <sus12@sunacwy.com.cn>
2022-01-01 14:12:11 +0800

    add API /isWorkingDay/:day

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
 	"net/http"
 	"sunacAttendance/config"
 )
+
+type DayInfo struct {
+	Code int `json:"code"`
+	Type struct {
+		Type int    `json:"type"`
+		Name string `json:"name"`
+		Week int    `json:"week"`
+	} `json:"type"`
+	Holiday struct {
+		Holiday bool   `json:"holiday"`
+		Name    string `json:"name"`
+		Wage    int    `json:"wage"`
+		Date    string `json:"date"`
+		Rest    int    `json:"rest"`
+	} `json:"holiday"`
+}
 
 type WechatToken struct {
 	AccessToken string `json:"access_token"` // 获取到的凭证
@@ -36,12 +53,49 @@ func main() {
 			"message": "pong",
 		})
 	})
+	/*获取指定日期的节假日信息
+	接口地址：http://timor.tech/api/holiday/info/$date
+	@params $date: 指定日期的字符串，格式 ‘2018-02-23’。可以省略，则默认服务器的当前时间。
+	@return json: 如果不是节假日，holiday字段为null。
+	{
+		"code": 0,              // 0服务正常。-1服务出错
+		"type": {
+			"type": enum(0, 1, 2, 3), // 节假日类型，分别表示 工作日、周末、节日、调休。
+			"name": "周六",         // 节假日类型中文名，可能值为 周一 至 周日、假期的名字、某某调休。
+			"week": enum(1 - 7)    // 一周中的第几天。值为 1 - 7，分别表示 周一 至 周日。
+		},
+		"holiday": {
+			"holiday": false,     // true表示是节假日，false表示是调休
+			"name": "国庆前调休",  // 节假日的中文名。如果是调休，则是调休的中文名，例如'国庆前调休'
+			"wage": 1,            // 薪资倍数，1表示是1倍工资
+			"after": false,       // 只在调休下有该字段。true表示放完假后调休，false表示先调休再放假
+			"target": '国庆节'     // 只在调休下有该字段。表示调休的节假日
+		}
+	}*/
+	router.GET("/isWorkingDay/:day", func(c *gin.Context) {
+		day := c.Param("day")
+		client := resty.New()
+		resp, err := client.R().
+			SetPathParam("day", day).
+			SetResult(&DayInfo{}).
+			SetHeader("Accept", "application/json").
+			Get("https://timor.tech/api/holiday/info/:day")
+		if resp.IsSuccess() {
+			dayInfo := resp.Result().(*DayInfo)
+			if dayInfo.Code == 0 {
+				c.JSON(http.StatusOK, dayInfo)
+			} else {
+				c.JSON(http.StatusOK, gin.H{"error": "服务出错"})
+			}
+		} else {
+			c.JSON(http.StatusOK, gin.H{"error": err})
+		}
+	})
 	// wechat
 	wechat := router.Group("/wechat")
 	wechat.GET("/userPhoneNumber", func(c *gin.Context) {
 		code := c.Query("code")
 		client := resty.New()
-		client.SetDebug(true)
 		resp, err := client.R().
 			SetQueryParams(map[string]string{
 				"appid":      "wxa1afc52994e69d4c",   // 小程序 appId
@@ -51,6 +105,7 @@ func main() {
 			SetResult(&WechatToken{}).
 			SetHeader("Accept", "application/json").
 			Get("https://api.weixin.qq.com/cgi-bin/token")
+		fmt.Println("  Status Code:", resp.StatusCode())
 		if resp.IsSuccess() {
 			token := resp.Result().(*WechatToken)
 			if token.ErrCode == 0 {
